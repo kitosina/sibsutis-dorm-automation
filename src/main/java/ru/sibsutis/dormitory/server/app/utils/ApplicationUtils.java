489d197393ed3f47f041ee51d902e282f54ea693
zakhlevnynm <n.m.zakhlevny@novotelecom.ru>
2021-09-12 20:18:50 +0700

    rel-1 Корректировка ApplicationUtils

--- src/main/java/ru/sibsutis/dormitory/server/app/utils/ApplicationUtils.java
+++ src/main/java/ru/sibsutis/dormitory/server/app/utils/ApplicationUtils.java
@@ -8,8 +8,7 @@ import java.io.InputStream;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-@SuppressWarnings({"checkstyle:hideutilityclassconstructor"})
-public final class ApplicationUtils {
+public abstract class ApplicationUtils {
 
     private static final Logger LOG = getLogger(Starter.class);
 
@@ -17,7 +16,7 @@ public final class ApplicationUtils {
      * @return Возвращает версию приложения
      */
     public static String getVersion() {
-        try (InputStream is = Starter.class.getResourceAsStream("/app.version")) {
+        try (InputStream is = ApplicationUtils.class.getResourceAsStream("/app.version")) {
             return IOUtils.toString(is);
         } catch (Exception e) {
             LOG.warn(e.getMessage(), e);
