a82dc59cdf35276bcf10b6d72b0ca68e1c92f5d0
zakhlevnynm <n.m.zakhlevny@novotelecom.ru>
2021-12-13 01:27:49 +0700

    rel-1 добавление шаблонов

--- src/main/java/ru/sibsutis/dormitory/server/app/config/WebConfig.java
+++ src/main/java/ru/sibsutis/dormitory/server/app/config/WebConfig.java
@@ -2,6 +2,8 @@ package ru.sibsutis.dormitory.server.app.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,6 +12,26 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(final FormatterRegistry registry) {
         WebMvcConfigurer.super.addFormatters(registry);
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/home_page").setViewName("home_page");
+        registry.addViewController("/kvitancii").setViewName("kvitancii");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/personal_data").setViewName("personal_data");
+        registry.addViewController("/registration").setViewName("registration");
+        registry.addViewController("/repair").setViewName("repair");
+        registry.addViewController("/spravki").setViewName("spravki");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
